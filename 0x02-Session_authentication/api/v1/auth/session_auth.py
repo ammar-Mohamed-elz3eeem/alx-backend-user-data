ec9bb022491f36c59f49ad3b47cda5b00750b524
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-12 08:09:37 +0200

    fix pycodestyle

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,7 +13,6 @@ class SessionAuth(Auth):
 
     user_id_by_session_id = {}
 
-
     def create_session(self, user_id: str = None) -> str:
         """create session for user_id and save it in the dictionary"""
         if type(user_id) is not str or user_id is None:
@@ -21,7 +20,7 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """retrieve user id using his session id"""
         if type(session_id) is not str or session_id is None:
