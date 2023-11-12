ba9dba2b7362f66a6859a2fa6d019288cfe4ed56
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-12 04:01:25 +0200

    implement authorization on users

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,6 +3,8 @@
 Route module for the API
 """
 from os import getenv
+
+from api.v1.auth.auth import Auth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -12,6 +14,26 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+
+if getenv("AUTH_TYPE", "") == "auth":
+    auth = Auth()
+
+
+@app.before_request
+def check_auth():
+    if auth is None:
+        return
+    if auth.require_auth(request.path,
+                         ['/api/v1/status/',
+                          '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/']) is False:
+        return
+    if auth.authorization_header(request) is None:
+        return abort(401)
+    if auth.current_user(request) is None:
+        return abort(403)
 
 
 @app.errorhandler(404)
