1d3fe32a5bfa20673b0fc1ea607cb41a863ba310
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-12 15:13:24 +0200

    edit UserSession to call super init method

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -12,5 +12,6 @@ class UserSession(Base):
 
     def __init__(self, *args: list, **kwargs: dict):
         """initialize new UserSession Object"""
+        super().__init__(*args, **kwargs)
         self.user_id = kwargs.get("user_id")
         self.session_id = str(uuid.uuid4())
