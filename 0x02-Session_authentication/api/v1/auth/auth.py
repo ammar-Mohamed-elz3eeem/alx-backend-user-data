ea8a2b8a488678bf120189818589c465e420e560
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-12 08:15:56 +0200

    implement session_cookie functionality

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -34,3 +35,11 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """get current user from the autherization key"""
         return None
+
+    def session_cookie(self, request=None):
+        if request is None:
+            return None
+        SESSION_NAME = getenv("SESSION_NAME", None)
+        if SESSION_NAME is None:
+            return None
+        return request.cookies.get(SESSION_NAME)
