778e199f616366caa8b2be6074fd54f1fcc8a535
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-14 16:35:14 +0200

    add logout and reset password functionality

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,6 +61,28 @@ class Auth:
         except Exception:
             return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """generate reset password token for user to make them
+        able to change thier passwords"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except Exception:
+            raise ValueError()
+
+    def update_password(self, reset_token: str, new_password: str) -> None:
+        """update user passwords after checking thier reset token
+        assigned to them"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id, hashed_password=bcrypt.hashpw
+                                 (new_password,
+                                  bcrypt.gensalt()))
+        except Exception:
+            raise ValueError()
+
 
 def _hash_password(passwd: str) -> str:
     """hash given password string using bcrypt"""
