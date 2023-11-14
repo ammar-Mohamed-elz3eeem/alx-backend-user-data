e970ab6aa02b4d775784dafcfe73eae5c0d49ee7
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-14 14:40:19 +0200

    add login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """hash password module"""
 from user import User
 from db import DB
-from flask import jsonify, Flask, request, abort
+from flask import jsonify, Flask, request, abort, Response
 from auth import Auth
 
 
@@ -31,6 +31,26 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
     return jsonify({"email": f"{email}", "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=True)
+def login():
+    """Users post reoute to login them in system"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except Exception:
+        return abort(400)
+
+    if AUTH.valid_login(email, password):
+        sess_id = AUTH.create_session(email)
+        res = Response(jsonify({"email": f"{email}",
+                                "message": "logged in"
+                                }),
+                       200).set_cookie("session_id", sess_id)
+        return res
+    else:
+        return abort(401)
 
 
 if __name__ == "__main__":
